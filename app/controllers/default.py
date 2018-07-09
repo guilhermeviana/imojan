7727aaf1306b21cf26760074a44c482d738f4972
guilhermeviana <guilhermeviana609@gmail.com>
2018-07-08 22:29:01 -0300

    Adicionando templates

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,17 +1,14 @@
 from app import app
 from flask import render_template
-
 from datetime import datetime
 from app.models.forms import LoginForm
-
 from app.models.tables import User, Homes
 from app.controllers.functions import ControlHomes
-
 from app import db,session,Session
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route("/login", methods=["POST","GET"])
 def login():
@@ -35,11 +32,13 @@ def login():
 
 @app.route("/republica/cadastro")
 def create():
+    form = LoginForm()
+    return render_template('create.html', form=form)
    # i = ControlHomes()  
   #  i.addHome(1,"gui", 23.23, "OLA", "1111111111", datetime.now(),"39480000", "SS","sdff", 2, "CASA")
-    k = ControlHomes()
-    j = k.listHomes()
-    for l in j:
-        return (l)
+   # k = ControlHomes()
+  #  j = k.listHomes()
+  #  for l in j:
+   #     return (l)
     
    
